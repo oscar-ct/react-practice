cbbe1d07ec222357a5c53995b0651d2c2a237a8c
Oscar Castro <oscar.a.castro818@gmail.com>
2023-02-27 21:10:40 -0600

    feat: completed walkthrough of initial react practice

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className='container'>
+            <h1>Hello from the app component</h1>
+            <p>Hey there</p>
+            <label htmlFor="main"/>
+        </div>
+    )
+}
+export default App;
