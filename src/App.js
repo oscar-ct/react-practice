0a34d5d381426cb186330662cf82a16a537b4f1d
Oscar Castro <oscar.a.castro818@gmail.com>
2023-02-27 23:17:39 -0600

    feat: created first component and added prop to function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Header from "./components/Header";
+
 function App() {
     const title = 'Blog Post';
     const body = 'This is my blog post';
@@ -12,21 +14,42 @@ function App() {
         }
 
     ];
+    const loading = false;
+    const showComments = true;
+    if (loading) {
+        return <h1>Loading....</h1>
+    }
+
+    const commentBlock = <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+            {comments.map(function (comment, index) {
+                return <li key={comment.id}>{comment.text}</li>
+            })}
+        </ul>
+    </div>;
+
     return (
-        <div className='container'>
-            <h1>Hello from the app component</h1>
-            <p>{title.toUpperCase()} :) {body}</p>
-            {Math.ceil(Math.random() * (5 + 5))}
-            <label htmlFor="main"/>
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map(function (comment, index) {
-                        return <li key={comment.id}>{comment.text}</li>
-                    })}
-                </ul>
+        <>
+            <Header text="Test UI"/>
+            <div className='container'>
+                <h1>Hello from the app component</h1>
+                <p>{title.toUpperCase()} :) {body}</p>
+                <p>{Math.ceil(Math.random() * (5 + 5))}</p>
+                <label htmlFor="main"/>
+                {showComments && commentBlock}
+                {/*{showComments ? (*/}
+                {/*    <div className="comments">*/}
+                {/*        <h3>Comments ({comments.length})</h3>*/}
+                {/*        <ul>*/}
+                {/*            {comments.map(function (comment, index) {*/}
+                {/*                return <li key={comment.id}>{comment.text}</li>*/}
+                {/*            })}*/}
+                {/*        </ul>*/}
+                {/*    </div>*/}
+                {/*) : 'no'}*/}
             </div>
-        </div>
-    );
+        </>
+    )
 }
 export default App;
