6521a451caed2e9ed85cb2114b049dbc5495004d
Oscar Castro <oscar.a.castro818@gmail.com>
2023-02-27 21:49:03 -0600

    feat: reviewed dymanic values and lists in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 function App() {
+    const title = 'Blog Post';
+    const body = 'This is my blog post';
+    const comments = [
+        {
+            id: 1,
+            text: 'These are comments'
+        },
+        {
+            id: 2,
+            text: "These are more comments"
+        }
+
+    ];
     return (
         <div className='container'>
             <h1>Hello from the app component</h1>
-            <p>Hey there</p>
+            <p>{title.toUpperCase()} :) {body}</p>
+            {Math.ceil(Math.random() * (5 + 5))}
             <label htmlFor="main"/>
+            <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map(function (comment, index) {
+                        return <li key={comment.id}>{comment.text}</li>
+                    })}
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
 export default App;
