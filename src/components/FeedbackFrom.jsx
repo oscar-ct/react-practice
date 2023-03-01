dcea85628336941d4f8559f9b94afd6bff5ab281
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-01 16:00:48 -0600

    feat: form validation

--- src/components/FeedbackFrom.jsx
+++ src/components/FeedbackFrom.jsx
@@ -5,13 +5,24 @@ function FeedbackFrom() {
 
 
     const [text, setText] = useState('');
-
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState("")
 
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text !== "" && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null);
+            setBtnDisabled(false)
+        }
       setText(e.target.value)
     }
 
-
+    // isDisabled={text.length > 10}
 
     return (
         <Card>
@@ -19,8 +30,12 @@ function FeedbackFrom() {
                 <h2>How would you rate your service?</h2>
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                    <Button type="submit" version='secondary' isDisabled='false'>Submit</Button>
+                    <Button type="submit" version='secondary'  isDisabled={btnDisabled}
+                    >Submit</Button>
                 </div>
+                {/*{message !== "" ?*/}
+                {/*    <div> {message} </div> : ""}*/}
+                {message && <div>{message}</div>}
             </form>
         </Card>
     )
