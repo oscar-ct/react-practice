05d72817826a8fe71b6a2f230ea8fa49ed6b9797
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-01 18:10:06 -0600

    added framer motion, animated ratings

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,19 +1,33 @@
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
+import {motion, AnimatePresence} from "framer-motion";
 
 function FeedbackList({ feedback, handleDelete2}) {
 
         if (!feedback || feedback.length === 0) {
             return <p>No feedback yet</p>
         }
-        return (
-            <div className="feedback-list">
+    return (
+        <div className="feedback-list">
+            <AnimatePresence>
             {feedback.map(function (item) {
-                console.log(item)
-                return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete2}/>
+                return (
+                    <motion.div key={item.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+                    <FeedbackItem key={item.id} item={item} handleDelete={handleDelete2}/>
+                    </motion.div>
+                )
             })}
-            </div>
+            </AnimatePresence>
+        </div>
     )
+    //     return (
+    //         <div className="feedback-list">
+    //         {feedback.map(function (item) {
+    //             console.log(item)
+    //             return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete2}/>
+    //         })}
+    //         </div>
+    // )
 }
 FeedbackList.propTypes = {
     feedback: PropTypes.arrayOf(
